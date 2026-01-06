dd2dbeeb6e1345206ebcc51c7492c70fc33bbb5c
HarshitaApankar <harshitaapankar@gmail.com>
2026-01-06 18:10:12 +0530

    Day 2: largest of two numbers

---
+++ Day 02/largest_of_two.c
@@ -0,0 +1,17 @@
+//Program to find the largest of two numbers
+
+#include <stdio.h>
+
+int main()
+{
+    int a,b;
+    printf("Enter two numbers:\n");
+    scanf("%d %d",&a,&b);
+
+    if(a>b)
+          printf("%d is larger",a);
+    else
+          printf("%d is larger",b);
+
+    return 0;
+}
